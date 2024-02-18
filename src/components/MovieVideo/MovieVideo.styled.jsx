0ebc3cc1a8c0d852da103a1a8b163d57374cae51
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2024-02-18 21:21:21 +0200

    Update MovieVideo.styled.jsx

--- src/components/MovieVideo/MovieVideo.styled.jsx
+++ src/components/MovieVideo/MovieVideo.styled.jsx
@@ -3,9 +3,22 @@ import styled from 'styled-components';
 export const StyledMovieVideo = styled.div`
   iframe {
     width: 100%;
-    max-width: 560px;
-    height: auto;
-    margin-top: 30px;
+    height: 150px;
+    max-width: 260px;
     border: 0;
+  }
+
+  @media (min-width: 768px) {
+    iframe {
+      height: 320px;
+      max-width: 560px;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    iframe {
+      height: 440px;
+      max-width: 760px;
+    }
   }
 `;
